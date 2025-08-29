d3ae644e86307c948fb440f05e6f5fad5d091b81
chenjintang-shrimp <jimmy163mail@163.com>
2025-08-29 12:35:32 +0000

    fix(database): 修改 BeatmapTagVote 模型并创建新表
    - 将 BeatmapTagVote 模型的表名从 "beatmap_tag_votes" 改为 "beatmap_tags"
    - 创建新的数据库迁移文件以替换错误的原迁移文件
    - 删除错误的迁移文件 "4a827ddba235_add_table_beatmap_tags.py"

--- app/database/beatmap_tags.py
+++ app/database/beatmap_tags.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 from sqlmodel import Field, SQLModel
 
 
-class BeatmapTagVote(SQLModel):
-    __tablename__ = "beatmap_tag_votes"  # pyright: ignore[reportAssignmentType]
-    tag_id: int | None = Field(primary_key=True, index=True, default=None)
-    beatmap_id: int | None = Field(primary_key=True, index=True, default=None)
+class BeatmapTagVote(SQLModel, table=True):
+    __tablename__: str = "beatmap_tags"
+    tag_id: int = Field(primary_key=True, index=True, default=None)
+    beatmap_id: int = Field(primary_key=True, index=True, default=None)
+    user_id: int = Field(primary_key=True, index=True, default=None)
