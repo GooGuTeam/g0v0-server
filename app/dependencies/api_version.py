15aab7f808b27d51e8dd16b6eee636be22904286
MingxuanGame <MingxuanGame@outlook.com>
2025-09-14 04:51:51 +0000

    feat(api): use `int` to hint `APIVersion`

--- app/dependencies/api_version.py
+++ app/dependencies/api_version.py
@@ -5,16 +5,12 @@ from typing import Annotated
 from fastapi import Depends, Header
 
 
-def get_api_version(version: str | None = Header(None, alias="x-api-version")) -> int:
+def get_api_version(version: int | None = Header(None, alias="x-api-version")) -> int:
     if version is None:
         return 0
-    try:
-        version_int = int(version)
-        if version_int < 1:
-            raise ValueError
-        return version_int
-    except ValueError:
-        raise ValueError("Invalid API version header")
+    if version < 1:
+        raise ValueError
+    return version
 
 
 APIVersion = Annotated[int, Depends(get_api_version)]
