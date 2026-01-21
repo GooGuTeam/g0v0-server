1a54f6f94aa4fd7639a8dd58058eb2d116005938
CloneWith <clonewith@qq.com>
2026-01-22 02:08:27 +0800

    feat(exception): mark RequestError as a data class and rewrite ctor

--- app/models/error.py
+++ app/models/error.py
@@ -1,4 +1,6 @@
+from dataclasses import dataclass
 from enum import Enum
+from typing import Any
 
 from fastapi import HTTPException
 
@@ -21,7 +23,9 @@ class ErrorType(Enum):
     """
     All possible error types that could be passed to the client.
 
-    Each entry is a tuple of the message key and status code, which should match the arguments of ErrorEntry.
+    Each entry is a tuple of the message key, status code and fallback message, which should match the arguments of ErrorEntry.
+
+    Remember to keep this enum in sync with frontend l10n implementation.
     """
 
     UNKNOWN = ("unknown", 500)
@@ -34,11 +38,45 @@ class ErrorType(Enum):
         return ErrorEntry(*self.value)
 
 
+@dataclass
 class RequestError(HTTPException):
     """
     A wrapper for major API errors to simplify response composition.
+
+    Attributes:
+        msg_key (str): The key of the error message for localization.
+        status_code (int): The status code should be responded, defaults to 422 to match osu!api's behavior.
+        fallback_msg (str): The fallback message for clients without localization support.
+
+    Args:
+        error_type (ErrorType): The error type to initialize from.
+        extra (dict[str, Any] | None): Details to include in the response.
+        status_code (int): Overrides the default one given by the error type.
+        headers (dict[str, str] | None): Will be attached to the response header.
     """
 
-    def __init__(self, error_type: ErrorType):
-        self.error_entry = error_type.get_instance()
-        super().__init__(self.error_entry.status_code, detail={"error": self.error_entry.msg_key})
+    msg_key: str
+    status_code: int = 422
+    fallback_msg: str = None
+
+    def __init__(
+        self,
+        error_type: ErrorType,
+        extra: dict[str, Any] | None = None,
+        *,
+        status_code: int | None = None,
+        headers: dict[str, str] | None = None,
+    ):
+        self.msg_key, self.status_code, self.fallback_msg = error_type
+
+        # Optional details
+        detail = {"error": self.msg_key}
+        if extra:
+            detail.update(extra)
+
+        # Fallback message
+        if self.fallback_msg:
+            detail.update({"message": self.fallback_msg})
+
+        final_status = status_code if status_code is not None else self.status_code
+        super().__init__(final_status, detail=detail, headers=headers)
