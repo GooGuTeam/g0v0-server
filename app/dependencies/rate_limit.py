3931b3ded3ddcdb765d334ded727bafd9700ae76
chenjintang-shrimp <jimmy163mail@163.com>
2026-02-09 09:21:31 +0800

    chore(deps): adapt fastapi-limiter 0.2.0 changes 适配 fastapi-limiter 0.2.0的新的API语法，剩余不变

--- app/dependencies/rate_limit.py
+++ app/dependencies/rate_limit.py
@@ -2,11 +2,12 @@ from app.config import settings
 
 from fastapi import Depends
 from fastapi_limiter.depends import RateLimiter
+from pyrate_limiter import Duration, Limiter, Rate
 
 if settings.enable_rate_limit:
     LIMITERS = [
-        Depends(RateLimiter(times=1200, minutes=1)),
-        Depends(RateLimiter(times=200, seconds=1)),
+        Depends(RateLimiter(limiter=Limiter(Rate(1200, Duration.MINUTE)))),
+        Depends(RateLimiter(limiter=Limiter(Rate(200, Duration.SECOND)))),
     ]
 else:
     LIMITERS = []
